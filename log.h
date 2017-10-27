c043d66ba307a83ec30414821f03a5cdb8a0f5da
pateryan <pe2ryan@uwaterloo.ca>
2017-10-27 18:36:51 -0400

    Refactor internals of `llog()` to remove switch
    Greatly improve the readability of the `llog()` internals, making it
    more extendible in the future also. Since the static string array in
    `log.c` can easily be added to, more log levels can be added with
    one line.
    
    I would feel bad if I didn't show this trick and I forgot when sending
    the last PR....

--- log.h
+++ log.h
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 
-#define LOG_INFO    (1 << 0)
-#define LOG_DEBUG   (1 << 1)
-#define LOG_WARNING (1 << 2)
-#define LOG_ERROR   (1 << 3)
-#define LOG_FATAL   (1 << 4)
+#define LOG_INFO    0
+#define LOG_DEBUG   1
+#define LOG_WARNING 2
+#define LOG_ERROR   3
+#define LOG_FATAL   4
 
 void set_global_log_config(int config);
 void llog(int level, FILE* stream, const char* format, ...);
